96ab9e5a4968cec3667924d607d406a820a164b4
Alberto Medellin <albertomedellin85@gmail.com>
2020-08-18 19:55:23 -0700

    Fixed international and japan jobs view

---
+++ src/components/JapJobs.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "../NewsJobsStyle.css";
+
+
+
+function JapJobs({japJobs}) {
+    return (
+        <div>
+            {japJobs.map(job => (
+                <p>
+                    Title:<b>{job.jobtitle}</b><br />
+                    Company:<b>{job.company}</b><br />
+                    {job.snippet}<br />
+                    Location:<b>{job.formattedLocationFull}</b><br />
+                    Post Date:<b>{job.date}</b><br />
+                    <a href={job.url}>Link to Original Post</a>
+                </p>
+            ))}
+        </div>
+    )
+}
+
+export default JapJobs
