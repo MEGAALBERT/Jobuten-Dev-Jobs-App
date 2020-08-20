60b8c6b4686dc7fc397c309ffeaa06deb9db218f
Eri Shimada <shimaeri515@gmail.com>
2020-08-20 14:39:04 +0900

    working on login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,57 @@
-import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
+import React, { useState } from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
+import axios from "axios";
 
 const Login = () => {
-return (
-<MDBContainer>
-  <MDBRow>
-    <MDBCol md="6">
-      <form>
-        <p className="h4 text-center mb-4">Sign in</p>
-        <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
-          Your email
-        </label>
-        <input type="email" id="defaultFormLoginEmailEx" className="form-control" />
-        <br />
-        <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
-          Your password
-        </label>
-        <input type="password" id="defaultFormLoginPasswordEx" className="form-control" />
-        <div className="text-center mt-4">
-          <MDBBtn color="indigo" type="submit">Login</MDBBtn>
-        </div>
-      </form>
-    </MDBCol>
-  </MDBRow>
-</MDBContainer>
-);
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  async function addUser(e) {
+    const user = {
+      username: username,
+      password: password,
+    };
+
+    console.log("!!!!", user);
+    await axios.post("/login", user);
+    e.preventDefault();
+  }
+
+  return (
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol md="6">
+          <form>
+            <p className="h4 text-center mb-4">Sign in</p>
+            <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+              Your username
+            </label>
+            <input
+              type="email"
+              id="defaultFormLoginEmailEx"
+              className="form-control"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <br />
+            <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+              Your password
+            </label>
+            <input
+              type="password"
+              id="defaultFormLoginPasswordEx"
+              className="form-control"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className="text-center mt-4">
+              <MDBBtn color="indigo" type="submit" onClick={addUser}>
+                Login
+              </MDBBtn>
+            </div>
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
 };
 
 export default Login;
